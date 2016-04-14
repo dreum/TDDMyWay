0b934316fd297b812805aa2afb75ad882848d04e
dreum <dirk.reum@gmail.com>
2016-04-14 05:46:38 -0500

    Added files via upload

---
+++ UnitTestSimulateGameOfLive/SimulateGameOfLife/SimulateWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimulateGameOfLife
+{
+    public class SimulateWorld
+    {
+        public List<Cell> Simulate()
+        {
+            return new List<Cell>() { Cell.Dead, Cell.Alive, Cell.Dead, Cell.Alive, Cell.Dead, Cell.Alive, Cell.Dead, Cell.Alive, Cell.Dead };
+        }
+    }
+}
---
+++ UnitTestSimulateGameOfLive/UnitTestSimulateGameOfLive/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimulateGameOfLife;
+
+namespace UnitTestSimulateGameOfLife
+{
+    [TestClass]
+    public class SimulateTest
+    {
+        public SimulateWorld _simulateWorld = new SimulateWorld();
+        [TestMethod]
+        public void SimulateReturnsDefaultWorld()
+        {
+            List<Cell> outputWorld = new List<Cell>(){ Cell.Dead, Cell.Alive, Cell.Dead, Cell.Alive, Cell.Dead, Cell.Alive, Cell.Dead, Cell.Alive, Cell.Dead };
+            CollectionAssert.AreEqual(outputWorld, _simulateWorld.Simulate());
+        }
+    }
+}
